c0dc7247743a812856cd894a16f8d5ef5f8bab3c
Djavid Gabibiyan <djavidoff@gmail.com>
2019-08-16 17:29:15 +0500

    Add first pending tests
    Co-authored-by: Konstantin Shuplenkov <k.shuplenkov@gmail.com>

---
+++ test/unit/utils/waitWithAttempts.spec.js
@@ -0,0 +1,6 @@
+describe('waitWithAttempts', () => {
+  it('should sequentially execute function selected number of attempts waiting for result');
+  it('should exit early if function returns a result');
+
+  it('should throw an error if function throws an error');
+});
