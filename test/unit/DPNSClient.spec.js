d49ad9d8fbd347eee9cf65d3bf394f8316278bba
Djavid Gabibiyan <djavidoff@gmail.com>
2019-08-24 11:46:01 +0500

    Test `DPNSClient` constructor and method call arguments

--- test/unit/DPNSClient.spec.js
+++ test/unit/DPNSClient.spec.js
@@ -1,6 +1,10 @@
 const Document = require('@dashevo/dpp/lib/document/Document');
+const Contract = require('@dashevo/dpp/lib/contract/Contract');
+
+const dpnsDocuments = require('@dashevo/dpns-contract/src/schema/dpns-documents');
 
 const DPNSClient = require('../../lib/DPNSClient');
+
 const dpnsDocumentFixture = require('../../lib/test/fixtures/getDpnsDocumentFixture');
 const InvalidArgumentError = require('../../lib/errors/InvalidArgumentError');
 
@@ -18,6 +22,22 @@ describe('DPNSClient', () => {
     dpnsClient.searchMethod = this.sinon.stub().resolves([parentDocument]);
   });
 
+  describe('#constructor', () => {
+    it('should set arguments correctly', () => {
+      const dapiClient = { name: 'dapiClient' };
+      const wallet = { name: 'wallet' };
+      const blockchainIdentity = { name: 'blockchainIdentity' };
+
+      dpnsClient = new DPNSClient(dapiClient, wallet, blockchainIdentity);
+
+      expect(dpnsClient.dapiClient).to.deep.equal(dapiClient);
+      expect(dpnsClient.wallet).to.deep.equal(wallet);
+      expect(dpnsClient.dpp.getContract()).to.deep.equal(
+        new Contract('dpnsContract', dpnsDocuments),
+      );
+    });
+  });
+
   describe('#register', () => {
     it('should throw an error if `name` is not specified', async () => {
       try {
@@ -31,7 +51,11 @@ describe('DPNSClient', () => {
     });
 
     it('should return a document', async () => {
-      const result = await dpnsClient.register('name');
+      const name = 'name';
+
+      const result = await dpnsClient.register(name);
+
+      expect(dpnsClient.registerMethod).to.have.been.calledWith(name);
 
       expect(result).to.be.an.instanceOf(Document);
       expect(result).to.equal(parentDocument);
@@ -51,7 +75,11 @@ describe('DPNSClient', () => {
     });
 
     it('should return a document', async () => {
-      const result = await dpnsClient.resolve('someName');
+      const name = 'someName';
+
+      const result = await dpnsClient.resolve(name);
+
+      expect(dpnsClient.resolveMethod).to.have.been.calledWith(name);
 
       expect(result).to.be.an.instanceOf(Document);
       expect(result).to.be.equal(parentDocument);
@@ -82,7 +110,14 @@ describe('DPNSClient', () => {
     });
 
     it('should return a document', async () => {
-      const result = await dpnsClient.resolveByRecord('record', 'value');
+      const recordName = 'record';
+      const recordValue = 'value';
+
+      const result = await dpnsClient.resolveByRecord(recordName, recordValue);
+
+      expect(dpnsClient.resolveByRecordMethod).to.have.been.calledWith(
+        recordName, recordValue,
+      );
 
       expect(result).to.be.an.instanceOf(Document);
       expect(result).to.equal(parentDocument);
@@ -113,7 +148,14 @@ describe('DPNSClient', () => {
     });
 
     it('should return an array of documents', async () => {
-      const result = await dpnsClient.search('labelPrefix', 'parentDomainName');
+      const labelPrefix = 'labelPrefix';
+      const parentDomainName = 'parentDomainName';
+
+      const result = await dpnsClient.search(labelPrefix, parentDomainName);
+
+      expect(dpnsClient.searchMethod).to.have.been.calledWith(
+        labelPrefix, parentDomainName,
+      );
 
       expect(result).to.be.an('array');
       expect(result).to.have.lengthOf(1);
