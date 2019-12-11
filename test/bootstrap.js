4d790eb3fd402fe553aafa5987c41278d3d8543b
Djavid Gabibiyan <djavidoff@gmail.com>
2019-12-11 21:35:19 +0500

    chore: update `bootstrap` for functional tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,7 +1,34 @@
+const path = require('path');
+
+const dotenvSafe = require('dotenv-safe');
+const dotenvExpand = require('dotenv-expand');
+
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const { expect, use } = require('chai');
 const dirtyChai = require('dirty-chai');
+
+const DriveApiOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/drive/api/DriveApiOptions',
+);
+const DriveUpdateStateOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/drive/updateState/DriveUpdateStateOptions',
+);
+
+const DashCoreOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/dashCore/DashCoreOptions',
+);
+
+const DapiCoreOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/dapi/core/DapiCoreOptions',
+);
+const DapiTxFilterStreamOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/dapi/txFilterStream/DapiTxFilterStreamOptions',
+);
+
+const MachineOptions = require(
+  '@dashevo/dp-services-ctl/lib/services/machine/MachineOptions',
+);
 
 use(dirtyChai);
 use(sinonChai);
@@ -17,5 +44,54 @@ beforeEach(function beforeEach() {
 afterEach(function afterEach() {
   this.sinon.restore();
 });
+
+const dotenvConfig = dotenvSafe.config({
+  path: path.resolve(__dirname, '..', '..', '.env'),
+});
+dotenvExpand(dotenvConfig);
+
+if (process.env.SERVICE_IMAGE_DRIVE) {
+  DriveApiOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_DRIVE,
+    },
+  });
+
+  DriveUpdateStateOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_DRIVE,
+    },
+  });
+}
+
+if (process.env.SERVICE_IMAGE_CORE) {
+  DashCoreOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_CORE,
+    },
+  });
+}
+
+if (process.env.SERVICE_IMAGE_DAPI) {
+  DapiCoreOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_DAPI,
+    },
+  });
+
+  DapiTxFilterStreamOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_DAPI,
+    },
+  });
+}
+
+if (process.env.SERVICE_IMAGE_MACHINE) {
+  MachineOptions.setDefaultCustomOptions({
+    container: {
+      image: process.env.SERVICE_IMAGE_MACHINE,
+    },
+  });
+}
 
 global.expect = expect;
