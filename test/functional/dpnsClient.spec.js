877523ba32cbb8fcde7b54a8f5a66f021ca393d1
Djavid Gabibiyan <djavidoff@gmail.com>
2019-12-26 14:34:35 +0500

    chore: update functional test

--- test/functional/dpnsClient.spec.js
+++ test/functional/dpnsClient.spec.js
@@ -226,14 +226,14 @@ describe('DPNS client', function main() {
     });
 
     const userDPNSClient = new DPNSClient(dapiClient, walletMock);
-    await userDPNSClient.register('wallet.dash', userIdentityMock, {
+    await userDPNSClient.register('wallet.dash.', userIdentityMock, {
       dashIdentity: userIdentityMock.getId(),
     });
 
-    const domainDocument = await userDPNSClient.resolve('wallet.dash');
+    const domainDocument = await userDPNSClient.resolve('wallet.dash.');
 
     expect(domainDocument.data.label).to.equal('wallet');
-    expect(domainDocument.data.normalizedParentDomainName).to.equal('dash');
+    expect(domainDocument.data.normalizedParentDomainName).to.equal('dash.');
     expect(domainDocument.data.records).to.deep.equal({
       dashIdentity: userIdentityMock.getId(),
     });
@@ -250,7 +250,7 @@ describe('DPNS client', function main() {
     expect(domainDocument.data.label).to.equal('dash');
     expect(domainDocument.data.normalizedParentDomainName).to.equal('');
     expect(domainDocument.data.records).to.deep.equal({
-      dashIdentity: userIdentityMock.getId(),
+      dashIdentity: dpnsUserIdentityMock.getId(),
     });
   });
 
