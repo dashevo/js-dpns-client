8b3de76a2545fb024244819a031524b04782e70e
Alex Werner <alex@werner.fr>
2020-02-03 17:01:09 +0100

    fix: path for karma spec (functional->distributable)

--- test.spec.js
+++ test.spec.js
@@ -1,4 +1,4 @@
 // This file is used for compiling tests with webpack into one file for using with karma
-const testsContext = require.context('./test/functional', true, /\.js$/);
+const testsContext = require.context('./test/distributable', true, /\.js$/);
 
 testsContext.keys().forEach(testsContext);
